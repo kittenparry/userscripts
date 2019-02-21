ccd89be8627a1bd0ac6e1a14420746c396a6236c
Edvin Boul <kittenparry@users.noreply.github.com>
2019-02-21 10:46:41 +0300

    Add Twitch Stop Autoplay

---
+++ twitch_stop_autoplay/tsa.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Twitch Stop Autoplay
+// @namespace    https://github.com/kittenparry/
+// @version      0.9
+// @description  Tries to stop autoplay on Twitch main page.
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @author       kittenparry
+// @match        *://www.twitch.tv
+// @grant        none
+// @license      MIT License
+// ==/UserScript==
+
+$( document ).ready(() => {
+	console.log('LOG: Loading page.');
+	var start_trying = setTimeout(() => {
+		var doc = document.querySelector('button[class="player-button qa-pause-play-button"]');
+		if(doc){
+			doc.click();
+			console.log('LOG: Stopped auto-play.');
+		}
+	}, 3000);
+});
+// 3000 might not be enough on some computers
+// needs testing and catching these problems
