da17975c1bda76185b68e40c919b02f3c2a3513e
Edvin Boul <kittenparry@users.noreply.github.com>
2020-04-09 15:31:58 +0300

    Fix bad url timestamp error (#21)
    The images flashes for a brief moment when bottom extra images load
    #2 returns the first image, again

--- instagram_image_viewer/iiv.user.js
+++ instagram_image_viewer/iiv.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram Image Viewer
 // @namespace    https://github.com/kittenparry/
-// @version      0.2.1
+// @version      0.3
 // @description  View the full image with the click of a button (or D key press).
 // @author       kittenparry
 // @match        https://www.instagram.com/p/*
@@ -10,21 +10,24 @@
 // ==/UserScript==
 
 /* CHANGELOG:
+ * 0.3:   fix bad url timestamp error
  * 0.2.1: use a var to store img url to save those bytes in script file size
  * 0.2:   fix the functionality | only gets the last image in image sets
  * 0.1:   initial
  */
 
-let img_url = document.documentElement.children[1].getElementsByTagName('script')[11].innerText.match(/"src":"(.*)","config_width":1080/)[1].split('src":')[document.documentElement.children[1].getElementsByTagName('script')[11].innerText.match(/"src":"(.*)","config_width":1080/)[1].split('src":').length - 1].replace(/[\\"]/g, '');
+window.addEventListener('load', () => {
+	let img_url = document.querySelector('img[class="FFVAD"]').src;
 
-window.addEventListener('keydown', (e) => {
-	var type = e.target.getAttribute('type');
-	var tag = e.target.tagName.toLowerCase();
-	if (type != 'text' && tag != 'textarea') {
-		if (e.keyCode == 68) {
-			window.location = img_url;
+	window.addEventListener('keydown', (e) => {
+		var type = e.target.getAttribute('type');
+		var tag = e.target.tagName.toLowerCase();
+		if (type != 'text' && tag != 'textarea') {
+			if (e.keyCode == 68) {
+				window.location = img_url;
+			}
 		}
-	}
-}, false);
+	}, false);
 
-document.body.innerHTML += '<a href="' + img_url + '" style="color: #7f8a94;"><div style="position: fixed; bottom: 50%; right: 0px; border-radius: 15px 0px 0px 15px; background: #faf0e6; padding: 13px; border: 2px solid #7f8a94; border-right-style: none;"><i style="border: solid black; border-width: 0 3px 3px 0; display: inline-block; padding: 3px; transform: rotate(45deg); -webkit-transform: rotate(45deg);"></i></div></a>';
+	document.querySelector('nav[class="NXc7H jLuN9  "]').innerHTML += '<a href="' + img_url + '" style="color: #7f8a94;"><div style="position: fixed; bottom: 50%; right: 0px; border-radius: 15px 0px 0px 15px; background: #faf0e6; padding: 13px; border: 2px solid #7f8a94; border-right-style: none;"><i style="border: solid black; border-width: 0 3px 3px 0; display: inline-block; padding: 3px; transform: rotate(45deg); -webkit-transform: rotate(45deg);"></i></div></a>';
+});
