b7e350078360d4050e7b5c67b94854018a02fff0
Edvin Boul <kittenparry@users.noreply.github.com>
2020-05-25 15:47:02 +0300

    Include redgifs.com

--- gfycat_hd_on/README.md
+++ gfycat_hd_on/README.md
@@ -1,3 +1,3 @@
-### Userscript to switch to HD on Gfycat after page load™
+### Userscript to switch to HD on Gfycat/redgifs after page load™
 
 [On Greasy Fork](https://greasyfork.org/en/scripts/392176-gfycat-hd-on)!
--- gfycat_hd_on/gho.user.js
+++ gfycat_hd_on/gho.user.js
@@ -1,10 +1,13 @@
 // ==UserScript==
-// @name         Gfycat HD On
+// @name         Gfycat/redgifs HD On
 // @namespace    https://github.com/kittenparry/
-// @version      0.1
+// @version      0.2
 // @description  Switch to HD version on page load™.
 // @author       kittenparry
-// @match        https://gfycat.com/*
+// @include      https://*.gfycat.com/*
+// @include      https://gfycat.com/*
+// @include      https://*.redgifs.com/*
+// @include      https://redgifs.com/*
 // @grant        none
 // @license      GPL-3.0-or-later
 // ==/UserScript==
